535c67632a9df1e2611648047cc37bc00bf7012d
Riley Wilton <meetingprince34@outlook.com>
2019-05-15 14:40:39 -0400

    Exit testing stage

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def checkIn(sid,data):
     sio.emit("checkedIn",{"ok":True},room=sid)
 
 @sio.on('checkOut')
-async def checkIn(sid,data):
+async def checkOut(sid,data):
     data=json.loads(data)
     checkedIn = False
     i=0
