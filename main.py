117ff447784ea3f8b848dac24a85933dc445767a
Riley Wilton <meetingprince34@outlook.com>
2019-06-08 21:16:11 -0400

    Started implementation of party status.
    Now lists party members

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from flask_socketio import SocketIO, emit, join_room
 parties = []    
 invites = []
 clients = []
+games = []
 clientDict = {}
 from multiprocessing import Process
 app = Flask(__name__)
@@ -29,8 +30,28 @@ def checkIn(data):
 @sio.on('disconnect')
 def disconnect():
     print("%s disconnected" % (request.sid))
-    del clients[clientDict[request.sid]]
     print(clients)
+    i=0
+    for item in parties:
+        i2=0
+        for itm in item["Members"]:
+            if itm[1] == request.sid:
+                del parties[i]["Members"][i2]
+                break
+            i2+=1
+        i+=1
+    i=0
+    for item in games:
+        i2=0
+        for itm in item["Members"]:
+            
+            if itm[2] == request.sid:
+                id = games[i]["id"]
+                del games[i]["Members"][i2]
+                sio.emit("playerJoined", games[i]["Members"], room=id)
+            i2+=1
+        i+=1
+    del clients[clientDict[request.sid]]
 
 @app.route("/")
 def index():
@@ -44,6 +65,8 @@ def startingParty(data):
     id = hashlib.sha224(os.urandom(16)).hexdigest()
     parties.append({"PartyID":id,"OwnerSID":sid,"OwnerUNAME":data[0],"Members":[[data[0],sid]]})
     sio.emit("hereIsID", {"id":id})
+
+
 @sio.on('invitingToParty')
 def invitingToParty(data):
     data=json.loads(data)
@@ -52,30 +75,77 @@ def invitingToParty(data):
         if item["OwnerSID"] == sid:
            invites.append({"username":data[0],"partyid":item["PartyID"],"OwnerUNAME":item["OwnerUNAME"]})
 
+
 @sio.on('joinParty')
 def joinParty(data):
-    partyId =  data[0]
+    partyID = data[0]
+    uname = data[1]
     i=0
-    party = ""
     for item in parties:
-        if item["PartyID"] == partyId:
-            parties[i]["Members"].append([data[1],request.sid])
-            party = item
+        if item["PartyID"] == partyID:
+            if item["OwnerSID"] != request.sid:
+                parties[i]["Members"].append([uname,request.sid])
+                for itm in item["Members"]:
+                    sio.emit("memberJoiningParty",[uname,item],room=itm[1])
         i+=1
-    if party != "":
-        for item in party["Members"]:
-            sio.emit("memberJoiningParty", [data[1],request.sid], room=item[0])
-        sio.emit("joiningParty", [True,party], room=request.sid)
-    else:
-        sio.emit("partyNotAvailible",[partyId],room=request.sid)
 
+@sio.on("leftParty")
+def leftParty(data):
+    i=0
+    for item in parties:
+        i2=0
+        for itm in item["Members"]:
+            if itm[0] == data:
+                del parties[i]["members"][i2]
+                sio.emit("memberJoiningParty", [data,item], room=item["PartyID"])
+            i2+=1
+        i+=1
 @sio.on("sendChat")
 def sendChat(data):
     sio.emit("newMsg", data, room=data["partyID"])
 
+
 @sio.on("room")
 def joinRoom(data):
     join_room(data)
+
+
+@sio.on("selectCharacter")
+def selectCharacter(data):
+    i=0
+    for item in clients:
+        if item["ClientUNAME"] == data["UNAME"]:
+            clients[i]["SelectedCharacter"] = data["CHARID"]
+            break
+        i+=1
+
+
+def getDefaultChar(uname):
+    pass
+
+
+@sio.on("startGame")
+def startGame(data):
+    print(data)
+    global games
+    games.append({"id":data[0],"Members":[]})
+    sio.emit("gameStarting","",room=data[0])
+
+
+@sio.on("joinGame")
+def joinGame(data):
+    partyId = data["ID"]
+    uname = data["UNAME"]
+    charid = data["CHARID"]
+    members = []
+    for game in games:
+        if game["id"] == partyId:
+            game["Members"].append([uname,charid,request.sid])
+            members = game["Members"]
+            print(members)
+    sio.emit("playerJoined", members, room=partyId)
+
+
 def reportInvites():
     while True:
         i=0
@@ -95,6 +165,8 @@ def reportInvites():
 success=True
 print(len(sys.argv))
 print(sys.argv)
+
+
 if __name__ == "__main__" and len(sys.argv) == 1:
     reportingThread = threading.Thread(target=reportInvites)
     reportingThread.start()
